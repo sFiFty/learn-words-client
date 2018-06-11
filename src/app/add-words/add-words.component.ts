3ec03346f4bc06eb3017426101a128fa60ea763b
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-11 08:19:32 +0300

    added home and words components

---
+++ src/app/add-words/add-words.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-words',
+  templateUrl: './add-words.component.html',
+  styleUrls: ['./add-words.component.css']
+})
+export class AddWordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

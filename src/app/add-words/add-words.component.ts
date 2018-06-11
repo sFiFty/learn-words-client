78d25ec6df93010617d92c84079e56369ed2229f
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-11 08:45:58 +0300

    migrated to sass

--- src/app/add-words/add-words.component.ts
+++ src/app/add-words/add-words.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-words',
   templateUrl: './add-words.component.html',
-  styleUrls: ['./add-words.component.css']
+  styleUrls: ['./add-words.component.scss']
 })
 export class AddWordsComponent implements OnInit {
 

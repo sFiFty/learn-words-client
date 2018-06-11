02ca1640eddf5b4e8803efa033cfd4ad8b737dd3
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-11 08:48:42 +0300

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AddWordsComponent } from './add-words/add-words.component';
+import { HomeComponent } from './home/home.component';
 
+const routes = [
+  { path: '', component: HomeComponent },
+  { path: 'add-word', component: AddWordsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddWordsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]

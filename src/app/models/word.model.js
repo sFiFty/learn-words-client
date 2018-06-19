10bfbf07f0a68e6b887476a3b8eb19ed8dcb988d
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2018-06-19 07:29:06 +0300

    added types

---
+++ src/app/models/word.model.js
@@ -0,0 +1,10 @@
+export class Word {
+  id: number;
+  Name: string;
+  Translation: string;
+  Date: date;
+}
+
+export class Translation {
+  translation: string
+}
--- src/app/words.service.ts
+++ src/app/words.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Word, Translation } from './models/word.model.js';
 import { Observable } from 'rxjs';
 
 const httpOptions = {
@@ -17,17 +19,17 @@ export class WordsService {
     return this._http.get('http://localhost:3000/api/Words');
   }
 
-  public addWord = data => {
+  public addWord = (data): Observable<Word> => {
     return this._http.post('http://localhost:3000/api/Words', data, httpOptions);
   }
 
-  public translate = (text) => {
+  public translate = (text: string): Observable<Translation> => {
     return this._http.post('http://localhost:3000/api/Words/translate', {
       text: text,
     });
   }
 
-  public deleteWord = id => {
+  public deleteWord = (id: number) => {
     return this._http.delete(`http://localhost:3000/api/Words/${id}`);
   }
 
